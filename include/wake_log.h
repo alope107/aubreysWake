d180914f9dda62ff4d22bd82f14bee37980ccb89
alope107 <auberonlopez@gmail.com>
2023-12-13 16:23:28 -0800

    Fix bug in wake logger

--- include/wake_log.h
+++ include/wake_log.h
@@ -7,9 +7,16 @@
 namespace wake {
     template<int maxSize=10000, typename... Types>
     inline void log(const Types&... vals) {
+        // Something bizarre is going on here...
+        // bn::to_string<maxSize>(" ") could just be of size 1, and it works if there's more than one parameter
+        // But it fails if there's just one???
+        // Maybe something with += vs = compiler optimization something something...
+        // This should probably be using an ostringstream anyway
         bn::string<maxSize> message = "";
-        (message += ... += (bn::to_string<maxSize>(vals) + bn::to_string<1>(" ")));
+        (message += ... += (bn::to_string<maxSize>(vals) + bn::to_string<maxSize>(" ")));
         bn::log(message);
     }
 }
+
+
 # endif // WAKELOG
